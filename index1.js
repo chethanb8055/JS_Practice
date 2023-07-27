43699507c37bd03a8da973bf64023d363418d03c
chethanb8055 <101992441+chethanb8055@users.noreply.github.com>
2023-07-27 23:01:15 +0530

    today practice -27-07

--- index1.js
+++ index1.js
@@ -1,93 +1,93 @@
-let Name = "chethan is good in Programming u chethan need to learn from him"
+// let Name = "chethan is good in Programming u chethan need to learn from him"
 
-let numbers = [1, 2, 3, 4, 5];
+// let numbers = [1, 2, 3, 4, 5];
 
-for(let number of numbers){
-    console.log(number+=1)
-}
-numbers =[];
-numbers.length=0
+// for(let number of numbers){
+//     console.log(number+=1)
+// }
+// numbers =[];
+// numbers.length=0
 
-console.log(numbers)
+// console.log(numbers)
 
-const NewNum =numbers.splice(2,0,'1c','b','a')
+// const NewNum =numbers.splice(2,0,'1c','b','a')
 
-console.log(NewNum)
-console.log(Name.startsWith("c"))
-console.log(Name.indexOf("need"))
-33
-console.log(Name.toUpperCase())
-CHETHAN IS GOOD IN PROGRAMMING U NEED TO LEARN FROM HIM
-console.log(Name.replace("chethan","car"))
-console.log(Name.split(" ").)
-console.log(Name)
+// console.log(NewNum)
+// console.log(Name.startsWith("c"))
+// console.log(Name.indexOf("need"))
+//33
+// console.log(Name.toUpperCase())
+//CHETHAN IS GOOD IN PROGRAMMING U NEED TO LEARN FROM HIM
+// console.log(Name.replace("chethan","car"))
+// console.log(Name.split(" ").)
+// console.log(Name)
 
-for(let i=0;i<5;i++){
+// for(let i=0;i<5;i++){
 
-}
+// }
 
-for(char of words){
+// for(char of words){
 
-}
-0
-for(key in any){
+// }
+// 0
+// for(key in any){
 
-}
-
-
-while(){
-
-}
-
-do{
+// }
 
 
-}while()
+// while(){
+
+// }
+
+// do{
 
 
-let anyList = [];
-let str =""
-
-let boole = true
+// }while()
 
 
-console.log(Array.isArray(anyList))
-console.log(typeof anyList)
+// let anyList = [];
+// let str =""
+
+// let boole = true
 
 
-setInterval(()=>{
-    console.log("chethan")
-},1000)
-
-setTimeout(() => {
-    console.log("chethan")
-}, 1000);
+// console.log(Array.isArray(anyList))
+// console.log(typeof anyList)
 
 
-let strList = "Hi,Guys".split("")
-console.log(strList)
-let arrList = [1,4,6,11,6]
+// setInterval(()=>{
+//     console.log("chethan")
+// },1000)
+
+// setTimeout(() => {
+//     console.log("chethan")
+// }, 1000);
 
 
-console.log(arrList.concat(strList))
+// let strList = "Hi,Guys".split("")
+// console.log(strList)
+// let arrList = [1,4,6,11,6]
 
 
-let arr = [12,32,45,67]
-let arr2 = [12,45,55,67,90,100]
-
-for(let i of arr){
-    for(let j of arr2 ){
-        if(j==i){
-            console.log(j)
-        }
-    }
-}
+// console.log(arrList.concat(strList))
 
 
-let str = "first_name"
+// let arr = [12,32,45,67]
+// let arr2 = [12,45,55,67,90,100]
 
-let arr =str.split("_")
-console.log(arr[0]+arr[1].charAt(0).toUpperCase() +arr[1].slice(1))
+// for(let i of arr){
+//     for(let j of arr2 ){
+//         if(j==i){
+//             console.log(j)
+//         }
+//     }
+// }
+
+
+// let str = "first_name"
+
+// let arr =str.split("_")
+// console.log(arr[0]+arr[1].charAt(0).toUpperCase() +arr[1].slice(1))
 
 
 
@@ -159,17 +159,530 @@ console.log(arr[0]+arr[1].charAt(0).toUpperCase() +arr[1].slice(1))
 // }
 // console.log(arr)
 
-let arr1 = [12,12,32,78]
+// let arr1 = [12,12,32,78]
 
 
-for(i=0 ; i<arr1.length;i++){
-    if(i==3){
-        arr1[i]=48
-    }
-}
+// for(i=0 ; i<arr1.length;i++){
+//     if(i==3){
+//         arr1[i]=48
+//     }
+// }
 
-console.log(arr1)
+// console.log(arr1)
 //  arr1 = [...arr1,12,32,21,...arr1]
 
 
 // console.log(arr1)
+
+
+// const str = "chethanB"
+
+// let newObj ={}
+// for(let char of str ){
+//     newObj[char]=newObj[char]?newObj[char]+1:1
+    
+// }
+
+
+// for
+// let news = Array.from(newObj)
+// console.log()
+// console.log(newObj)
+
+// let arr = [12,44,66,11,12]
+
+
+// for(let i=arr.length-1;i>0;i-- ){
+    
+//     if(i>2){
+//         arr[i+1]=arr[i]
+//     }
+//     if(i==2){
+//         arr[i]=34
+//     }
+// }
+// console.log(arr)
+
+// arr.length =5
+// console.log(arr)
+
+// let arr = [12,44,66,11,12]
+
+
+
+// for(i=0;i<arr.length-1;i++){
+//     if(i>1){
+//         arr[i]=arr[i+1]
+//     } 
+// }
+  
+
+
+// console.log(arr[Math.floor()
+
+// console.log(arr)
+
+
+// function add(a,b){
+//  return a+b
+
+// }
+
+
+// const add1 =(a,b)=>{
+//     return a+b
+// }
+
+
+// const sum  = add(23,12)
+// const sum1 = add1(32,34)
+// console.log(sum)
+// console.log(sum1)
+
+
+// let arr = [12,4,2,3,2,1]
+
+//  let num =  arr.reduce((a,b)=>{
+//     return a-b
+// },12)
+// console.log(num)
+
+
+// let obj = {
+//     name:"chetan",
+//     id:12
+// }
+
+// let obj2 =Object.assign({},obj)
+
+// console.log()
+
+// let a =0
+// let b =0
+// function add(...res){
+//     console.log(res)
+// }
+
+
+// let con = add(a,b)
+
+
+// (function(){
+//     console.log("Hi im callig this");
+// })();
+
+// (() => {
+//     console.log("hi, sir");
+// })();
+
+// (() => {
+//     var name1 = "chethan"
+//     console.log("hi, sir i got you ",name1);
+// })();
+// console.log(name1)
+
+
+// class vehicals {
+//     constructor (name,id,place){
+//         this.name =name
+//         this.id =id
+//         this.place = place
+//     }
+    
+//     name1(){
+//         console.log(`Your Name is ${this.name} and the identity is ${this.id}`)
+// }
+
+//
+
+
+
+
+// console.log(typeof )
+// let name = Symbol("chethan")
+// console.log(typeof name)
+
+
+// let useInfo ={
+
+//     [Symbol(name)] :"chethan"
+// }
+
+
+// let useOne = "chethan"
+
+
+// let obj = {
+//     name1:"chethan ",
+//     id : "12"
+// }
+
+// let newObgj = 
+
+// const newPerson = new Map()
+
+// newPerson.set("firstName","Chethahn")
+// newPerson.set("lastName","Hindi")
+// console.log(newPerson)
+
+
+// newPerson.get("lastName") = "chethan"
+// console.log(newPerson.get("lastName"),newPerson.has("lastName"),newPerson.set(true,'go'))
+// // newPerson.set("firstName","chethan")
+// for(let i of newPerson){
+//     console.log(i)
+// }
+
+// console.log(typeof  )
+
+
+
+
+// let id = "id"
+
+
+
+
+// for(let i of Object.entries(obj)){
+//     console.log(`the value of the object is ${i[0]} and the key is ${i[1]}`)
+//     // console.log(i[1],i[0])
+// }
+
+
+
+// console.log(obj.name1,obj['name1'],obj[`${id}`].n)
+
+
+
+// let useTwo =    "chethan"
+
+
+// let useSym = Symbol("chethan") 
+// let useSym2 = Symbol("chethan")
+
+
+// console.log(useSym===useSym2)
+
+// console.log(us)
+
+
+// optiona chaining
+
+
+// let obj = {
+//     name1:"chethan ",
+//     id : "12",
+//     Frind:{fistFrdn:"harish",SecFriend:"chethan"}
+
+
+// }
+
+
+// console.log()
+
+
+
+// for (let i = 0; i < arrList.length; i++) {
+    //     // const element = array[i];
+//     for(let j =0;j<arrList.length-1;j++){
+//         if(arrList[j]<arrList[j+1]){
+//             [arrList[j],arrList[j+1]]=[arrList[j+1],arrList[j]]
+//         }
+//     }
+
+// }
+
+// let arrList =[12,34,32,12,34,54,66]
+
+// let max =0;
+// let second =0
+// let third =0
+// let fouth =0
+// for(i=0;i<arrList.length;i++){
+//         if(max<arrList[i]){
+//             fouth =third
+//             third =second
+//             second =max
+//             max=arrList[i]
+//         }
+// }
+
+// for(i=0;i<i.length;i++){
+    
+
+// }
+
+
+// console.log(max,second,third,fouth)
+// // console.log(arrList[1])
+// console.log("name");
+
+
+
+// function name (name=this.name){
+//     console.log(`Person name is ${name} and His ID is ${this.id}`)
+// }
+
+// let person1 = {
+//     name:"chethan"
+//     ,id :12
+//     ,useInf:name
+//     ,check :function(){
+//         console.log(window ===this)
+//     }
+//     ,useInfo:function(song,dream){
+//         console.log(`name is ${this.name} and the id is ${this.id} and ${song} and ${dream}`)
+//     }
+//     ,add:function(a,b){
+//         console.log(a,b)
+//     }
+    
+// }
+// let person2 = {
+//     name:"chethan"
+//     ,id :12
+//     ,useInf:name   
+// }
+
+// let person3 = {
+//     name:"chethan3"
+//     ,id :121
+//     ,useInf:name
+    
+// }
+
+// person1.useInfo.call(person3,"AppuSong","Developer")
+// person1.useInfo.apply(person3,["AppuSong","Developer"])
+// console.log(person1.add.call(person3,12,12))
+// const newPerson = person1.useInfo.bind(person3)
+// newPerson("AppuSong","Developer")
+
+
+// class vehicals {
+//     constructor(name){
+//         this.name =name
+//     }
+// }
+
+// let fuc =(a)=>a=
+// console.log(typeof fuc,fuc())
+
+// console.log(typeof vehicals)
+
+
+// console.log(this===window)
+
+// // person1.check()
+
+// person1.useInf("Harish")
+// person2.useInf("Harish3")
+// person3.useInf("Harish4")
+
+// // person1.useInf("Harish")
+
+
+
+// "use strict"
+// let sum1 =function (a){
+//     name1 = a+10
+//     // console.log(this)
+//     console.log(name1)
+// }
+
+
+
+// sum1(2)
+// console.log(sum1(2,2))
+
+
+
+// let person3 = {
+//     name:"chethan3"
+//     ,id :121
+//     ,useInf:name
+    
+// }
+
+
+// let ob1 = Object.create(person3)
+
+// // let ob2 = Object.getPrototypeOf(ob1)
+
+// // Object.setPrototypeOf(ob2,person3)
+// // ob1.proto =
+// console.log(ob1.__proto__)
+// console.log(ob1)
+
+
+// function name (){
+//     useName ="chethan"
+// }
+
+
+// // name.age ="32"
+// name.prototype.age =32
+// name.prototype.age1 =43
+
+// console.log(name.prototype.age)
+
+
+// function name(){
+//     let useInfo =Object.create(name.prototype)
+
+//     useInfo.name ='chethn'
+//     useInfo.id =12
+//     return useInfo
+
+// }
+
+// // name.cal()
+
+
+// let returnValue = name()
+// name.prototype.name1 = function(){
+//  console.log("yes calling")
+
+// }
+
+
+// console.log(returnValue.name1())
+
+
+// console.log(returnValue.name1())
+
+
+// let name = {}
+
+
+// let name2 = new String('name')
+// // name2.indexOf(name2)
+// let name3 = new Array(1,3,4)
+// console.log(typeof name2)
+// console.log(Object.getPrototypeOf(name))
+
+
+
+// class Animals {
+
+//     constructor(name,age,id){
+//         this.name =name;
+//         this.age = age
+//         this.id = id
+//     }
+
+
+//     isSoCute(){
+//         return this.age<6
+//     }
+
+
+//     isSingSong(){
+//         return true
+//     }
+// }
+
+
+
+
+// class Human extends Animals{
+//     constructor(name,age,id){
+//         super(name,age,id)
+        
+
+//     }
+//    get isSoCute(){
+//         return "bad"
+//     }
+    
+   
+
+
+// }
+
+
+
+
+// let Dog = new Animals("Tommy",12)
+// let Navin = new Human("Navin",45,12)
+// Human.isName ="Chethan B Hindi"
+// console.log(Navin.isName)
+// console.log(Navin.SetProperty("Yash",45))
+// console.log(Dog.isSoCute(),Dog.isSingSong())
+
+// function sum(a){
+//     return function(b){
+//         return function(c){
+//             return a+b+c
+//         }
+//     }
+// }
+
+// let sum1 =a=>b=>c=>d=>a+b+c+d
+
+
+// console.log(sum1(1)(2)(3)(4))
+
+// console.log(sum(12)(12)(34))
+
+
+let arr1= [12,23,54,78,56,32]
+
+let arr2 =[34,78,12,34]
+
+let arr3 =[]
+// for(let i=0;i<arr1.length;i++){
+//     arr3[i]=arr1[i]
+// }
+// for(let i=0;i<arr2.length;i++){
+//     arr3[arr1.length+i]=arr2[i]
+// }
+let i =0
+// while(arr2.length-1>i){
+//     if(arr2[i]<arr2[i]{})
+//     i++
+//     // arr3[i]=arr1[i]
+//     // arr3[arr1.length+i] =arr2[i]
+//     // i++
+
+// }
+// arr3.length =10
+// console.log(arr2)
+
+// for(let i=0;i<arr1.length;i++){
+//     if(i==1){
+//         // console.log(i)
+//         continue
+//     }else if(i==4){
+//         console.log(i)
+//         break
+//     }else{
+//         console.log(i)
+//     }
+// }
+
+
+// console.log(arr3)
+
+// let arr2 =[1,2]
+// let arr1 = [1,2,]
+
+// let news = arr1.every((e)=>arr2.includes(e))
+// console.log(news)
+
+
+let fax =1;
+function sum(a){
+    // console.log(a)
+    // if(a<10){
+    //     sum(a+1)
+    //     // return 10 +a
+    // }    
+    if(a<6){
+     fax=fax*a   
+     sum(a+1)
+    }
+    return fax
+
+}
+
+console.log(sum(1))
